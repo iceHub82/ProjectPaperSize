6c3aad23d6b8ba053c4cca6cdf9431057b29d7db
Örvar Karlsson <orvar@secure-aid.com>
2025-01-22 16:46:03 +0100

    Darkmode + js update

--- pages.js
+++ pages.js
@@ -1,37 +1,38 @@
+var page = document.querySelector('page')
+
 // Handle changes to the page size radio buttons
 document.querySelectorAll('input[type=radio][name=pageSize]').forEach(function (radio) {
   radio.addEventListener('change', function () {
-    var page = document.querySelector('page');
     switch (this.value) {
       case 'A5':
-        page.setAttribute('size', 'A5');
+        page.setAttribute('size', 'A5')
         break;
       case 'A4':
-        page.setAttribute('size', 'A4');
+        page.setAttribute('size', 'A4')
         break;
       case 'A3':
-        page.setAttribute('size', 'A3');
+        page.setAttribute('size', 'A3')
         break;
     }
-  });
-});
+  })
+})
 
 // Handle changes to the page layout radio buttons
 document.querySelectorAll('input[type=radio][name=pageLayout]').forEach(function (radio) {
   radio.addEventListener('change', function () {
-    var page = document.querySelector('page');
     switch (this.value) {
       case 'portrait':
-        page.setAttribute('layout', 'portrait');
-        break;
+        page.setAttribute('layout', 'portrait')
+        break
       case 'landscape':
-        page.setAttribute('layout', 'landscape');
-        break;
+        page.setAttribute('layout', 'landscape')
+        break
     }
-  });
-});
+  })
+})
 
 // Handle changes to the dark mode toggle switch
 document.querySelector('#darkmodeSwitch').addEventListener('change', function () {
-  document.body.classList.toggle('dark-body');
-});
+  document.body.classList.toggle('dark-body')
+  page.classList.toggle('dark-page')
+})
